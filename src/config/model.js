2723e7f961e4091b8ea7d20c070dbf362c23e5ba
Shubham Kamath <dev.shubhaem@gmail.com>
2019-11-12 19:16:38 +0530

    Data model

---
+++ src/config/model.js
@@ -0,0 +1,9 @@
+let tdTitle = {'React' : 1, 'Vue' : 2, 'Angular' : 3};
+let tdValue = {1 : 'One Way', 2 : 'Two Way', 3 : 'Two Way'};
+let addPrompt = false;
+
+export default {
+    tdTitle : tdTitle,
+    tdValue : tdValue,
+    addPrompt : addPrompt
+}
